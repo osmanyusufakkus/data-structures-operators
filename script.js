4509b90b2afdd64e163d52c22416f6a0d6369bae
osmanyusufakkus <osmanyusufakkus@gmail.com>
2022-09-13 21:58:08 +0300

    Start new challenge

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//const flights =
+//  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -45,7 +46,81 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+*/
+////////////////////////////////////////////////////////////////
+/***************************CODING CHALLENGE #1***************************/
 
+const game = {
+  team1: 'GS',
+  team2: 'FB',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martines',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lwandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Levandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+/*
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni',
+};
+//Or assignment operator
+//rest1.numGuests = rest1.numGuests || 10;
+//rest2.numGuests = rest2.numGuests || 10;
+//rest1.numGuests ||= 10;
+//rest2.numGuests ||= 10;
+
+//Nulish assignment operator(null and undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+//And assignment opertor
+//rest1.owner = rest1.owner && '<ANONYMOUS>';
+//rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+*/
+/*
 restaurant.numGuest = 0;
 
 const guest = restaurant.numGuest || 11;
@@ -54,7 +129,7 @@ console.log(guest);
 //Nulish: null and undefined
 const guestCorrect = restaurant.numGuest ?? 10;
 console.log(guestCorrect);
-
+*/
 /*
 console.log('-----OR-----');
 //Use any data type, return any data type, short-circuiting
